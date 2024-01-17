d0ba9d5ff065d72e82c65496cd79b91e7bced28d
Dhruvin Thakkar <dhruvinthakkar248@gmail.com>
2024-01-17 10:45:25 +0530

    First commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import img from "../assets/img/hero.png";
+
+const Home = () => {
+  return (
+    <div className=" min-h-screen flex flex-col justify-center lg:flex-row items-center lg:justify-between lg:px-32 px-5 pt-24 lg:pt-10 bg-PrimaryColor">
+      {/* content section  */}
+      <div className=" space-y-4">
+        <h1 className=" text-5xl font-semibold leading-tight text-ExtraDarkColor w-full lg:w-3/4">
+          Fashion Forward, Earth Friendly
+        </h1>
+        <p className=" w-full lg:w-3/4 text-DarkColor font-medium">
+          A fashion website for sustainable future.
+        </p>
+
+        <button className=" bg-ExtraDarkColor text-white px-4 py-2 font-medium active:bg-amber-800">
+          shop with us
+        </button>
+      </div>
+
+      {/* img section */}
+      <div className=" bg-DarkColor p-5 rounded-tr-[290px] rounded-tl-3xl rounded-br-3xl mt-10">
+        <img className=" -ml-5 -mb-5" width={680} src={img} alt="img" />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
